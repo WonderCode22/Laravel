bf665bfd21d3d8be1d383ad16439e598f9443875
YukiMinami22 <phoenix22@tutanota.com>
2017-07-10 03:42:11 -0700

    Login & Register User

---
+++ UserManage/resources/views/verification.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="panel panel-default">
+        <div class="panel-heading">Registration</div>
+        <div class="panel-body">Your email is verified. An email is sent to you for verification</div>
+    </div>
+  </div>
+@endsection
